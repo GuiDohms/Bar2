5a2a89468626956bba29bee78b2fc08e2bd2352e
GuiDohms <guilherme.dohms@acad.pucrs.br>
2017-04-04 20:57:39 -0300

    a

---
+++ src/Client.java
@@ -0,0 +1,21 @@
+   
+public class Client {
+	private String name, cpf;
+	private int age;
+	private char gender;
+	
+	public Client(String name, String cpf, int age, char gender){
+		this.name = name;
+		this.cpf = cpf;
+		this.age = age;
+		this.gender = gender;
+	}
+	
+	public String toString(){
+		return("Client info: "+ '\n' +
+				"Name: " + name + '\n' +
+				"CPF: " + cpf + '\n' +
+				"Age: " + age + '\n' +
+				"Gender: " + gender);
+	}
+}
---
+++ src/ClientAssociate.java
@@ -0,0 +1,17 @@
+ 
+public class ClientAssociate extends Client {
+	private String associateNum;
+	
+	public ClientAssociate(String name, String cpf, int age, char gender, String associateNum) {
+		super(name, cpf, age, gender);
+		this.associateNum = associateNum;
+	}
+	
+
+	
+	
+		
+		
+	
+
+}
